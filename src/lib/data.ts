6696b797b23f94ce54710120f0d746a52973cc1b
Luis Martínez <lemartinezmendoza@gmail.com>
2023-11-09 17:49:34 -0500

    feat: add events section and rating, card components

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,4 +1,5 @@
 import type { LatLngExpression } from "leaflet";
+import cardImage1 from "../assets/card-1.jpg";
 
 interface CampingSpot {
   id: number;
@@ -25,5 +26,30 @@ export const campingSpotsData: CampingSpot[] = [
     place: "Pyokmart",
     campName: "ExploraTerra Base Camp",
     latLon: [93, 349],
+  },
+];
+
+interface CardData {
+  image: any;
+  title: string;
+  rating: number;
+  description: string;
+  views: string;
+  likes: string;
+  price: string;
+  priceUnit: string;
+}
+
+export const cardsData: CardData[] = [
+  {
+    image: cardImage1,
+    title: "Special Private Tour",
+    rating: 4,
+    description:
+      "Embark on an extraordinary cosmic camping experience. Whether it's a romantic getaway, a family adventure, or a solo exploration, our private tours offer an unforgettable escape. Explore alien landscapes at your pace, blending comfort and exploration. Your stellar journey awaits, creating timeless memories beyond Earth.",
+    views: "1.8k",
+    likes: "1.2k",
+    price: "499.99",
+    priceUnit: "person",
   },
 ];
